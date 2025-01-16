91331c0347f650d4e20011e614f4a5d894706db2
Zahar-Borovskiy <borovskiyzahar@gmail.com>
2025-01-16 13:53:48 -0300

    more Mixin

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -35,38 +35,26 @@ from .utils import calculate_birthday_countdown
 #     return render(request, 'birthday/birthday.html', context)
 
 
-# Создаём миксин.
 class BirthdayMixin:
     model = Birthday
+    success_url = reverse_lazy('birthday:list')
+
+
+class BirthdayFormMixin:
     form_class = BirthdayForm
     template_name = 'birthday/birthday.html'
-    success_url = reverse_lazy('birthday:list')
 
 
-# Добавляем миксин первым по списку родительских классов.
-class BirthdayCreateView(BirthdayMixin, CreateView):
-    # Не нужно описывать атрибуты: все они унаследованы от BirthdayMixin.
+class BirthdayCreateView(BirthdayMixin, BirthdayFormMixin, CreateView):
     pass
 
 
-class BirthdayUpdateView(BirthdayMixin, UpdateView):
-    # И здесь все атрибуты наследуются от BirthdayMixin.
+class BirthdayUpdateView(BirthdayMixin, BirthdayFormMixin, UpdateView):
     pass
 
 
-# Наследуем класс от встроенного ListView:
-class BirthdayListView(ListView):
-    # Указываем модель, с которой работает CBV...
-    model = Birthday
-    # ...сортировку, которая будет применена при выводе списка объектов:
-    ordering = 'id'
-    # ...и даже настройки пагинации:
-    paginate_by = 10 
-
-
-class BirthdayDeleteView(DeleteView):
-    model = Birthday
-    success_url = reverse_lazy('birthday:list')
+class BirthdayDeleteView(BirthdayMixin, DeleteView):
+    pass
 
 
 def delete_birthday(request, pk):
