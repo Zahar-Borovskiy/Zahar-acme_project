d93bfa936cea2528a12d73489be955558534445a
Zahar-Borovskiy <borovskiyzahar@gmail.com>
2025-01-16 14:22:48 -0300

    removed all unnecessary

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,38 +1,10 @@
 # birthday/views.py
-from django.shortcuts import get_object_or_404, redirect, render
 
 from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 from django.urls import reverse_lazy
 
-from django.core.paginator import Paginator
-
 from .forms import BirthdayForm
 from .models import Birthday
-from .utils import calculate_birthday_countdown
-
-
-# Добавим опциональный параметр pk.
-# def birthday(request, pk=None):
-#     if pk is not None:
-#         instance = get_object_or_404(Birthday, pk=pk)
-#     else:
-#         instance = None
-#     form = BirthdayForm(
-#         request.POST or None,
-#         # Файлы, переданные в запросе, указываются отдельно.
-#         files=request.FILES or None,
-#         instance=instance
-#     )
-#     # Остальной код без изменений.
-#     context = {'form': form}
-#     # Сохраняем данные, полученные из формы, и отправляем ответ:
-#     if form.is_valid():
-#         form.save()
-#         birthday_countdown = calculate_birthday_countdown(
-#             form.cleaned_data['birthday']
-#         )
-#         context.update({'birthday_countdown': birthday_countdown})
-#     return render(request, 'birthday/birthday.html', context)
 
 
 class BirthdayMixin:
@@ -57,18 +29,11 @@ class BirthdayDeleteView(BirthdayMixin, DeleteView):
     pass
 
 
-# def delete_birthday(request, pk):
-#     # Получаем объект модели или выбрасываем 404 ошибку.
-#     instance = get_object_or_404(Birthday, pk=pk)
-#     # В форму передаём только объект модели;
-#     # передавать в форму параметры запроса не нужно.
-#     form = BirthdayForm(instance=instance)
-#     context = {'form': form}
-#     # Если был получен POST-запрос...
-#     if request.method == 'POST':
-#         # ...удаляем объект:
-#         instance.delete()
-#         # ...и переадресовываем пользователя на страницу со списком записей.
-#         return redirect('birthday:list')
-#     # Если был получен GET-запрос — отображаем форму.
-#     return render(request, 'birthday/birthday.html', context)
+# Наследуем класс от встроенного ListView:
+class BirthdayListView(ListView):
+    # Указываем модель, с которой работает CBV...
+    model = Birthday
+    # ...сортировку, которая будет применена при выводе списка объектов:
+    ordering = 'id'
+    # ...и даже настройки пагинации:
+    paginate_by = 10
