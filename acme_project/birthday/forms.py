51a80e723058f0b17759d66305ee18a5ae35acf6
Zahar-Borovskiy <borovskiyzahar@gmail.com>
2025-01-20 14:54:44 -0300

    configured login and logout for user

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -4,6 +4,8 @@ from django import forms
 # Импортируем класс ошибки валидации.
 from django.core.exceptions import ValidationError
 
+from django.core.mail import send_mail
+
 # Импортируем класс модели Birthday.
 from .models import Birthday
 
@@ -24,7 +26,7 @@ class BirthdayForm(forms.ModelForm):
         widgets = {
             'birthday': forms.DateInput(attrs={'type': 'date'})
         }
-    def clean_first_name(self):
+    def clean_first_name(self): 
         # Получаем значение имени из словаря очищенных данных.
         first_name = self.cleaned_data['first_name']
         # Разбиваем полученную строку по пробелам 
@@ -32,11 +34,19 @@ class BirthdayForm(forms.ModelForm):
         return first_name.split()[0]
 
     def clean(self):
-        # Вызов родительского метода clean.
         super().clean()
         first_name = self.cleaned_data['first_name']
         last_name = self.cleaned_data['last_name']
         if f'{first_name} {last_name}' in BEATLES:
+            # Отправляем письмо, если кто-то представляется 
+            # именем одного из участников Beatles.
+            send_mail(
+                subject='Another Beatles member',
+                message=f'{first_name} {last_name} пытался опубликовать запись!',
+                from_email='birthday_form@acme.not',
+                recipient_list=['admin@acme.not'],
+                fail_silently=True,
+            )
             raise ValidationError(
                 'Мы тоже любим Битлз, но введите, пожалуйста, настоящее имя!'
-            )
+            ) 
